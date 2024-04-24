0dcd20b5389470481cfb555017c7d381cd8e3755
metadru1d <adamdelsolrun@gmail.com>
2024-04-24 19:36:43 -0400

    wallet connect complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [buttonText, setButtonText] = useState('Connect Wallet');
+
+  const isPhantomInstalled = window.phantom?.solana?.isPhantom
+
+  const getProvider = () => {
+    if ('phantom' in window) {
+      const provider = window.phantom?.solana;
+
+      if (provider?.isPhantom) {
+        return provider;
+      }
+    }
+
+    window.open('https://phantom.app', '_blank');
+  }
+
+  const connect = async () => {
+    const provider = getProvider();
+    try {
+      const resp = await provider.connect({ onlyIfTrusted: true });
+      console.log(resp.publicKey.toString());
+      setButtonText('Connected');
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={connect}>         
+          {buttonText}
         </button>
-        </div>
+      </div>
     </>
   )
 }
